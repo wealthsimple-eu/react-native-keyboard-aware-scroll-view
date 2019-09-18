bfa471d0a409f93a952d1dc46f1d0c0d3f349de9
Brian Mullan <bmullan91@gmail.com>
2019-09-18 17:59:39 +0100

    move index.js into lib

---
+++ build/index.js
@@ -0,0 +1,5 @@
+import listenToKeyboardEvents from './KeyboardAwareHOC';
+import KeyboardAwareScrollView from './KeyboardAwareScrollView';
+import KeyboardAwareFlatList from './KeyboardAwareFlatList';
+import KeyboardAwareSectionList from './KeyboardAwareSectionList';
+export { listenToKeyboardEvents, KeyboardAwareFlatList, KeyboardAwareSectionList, KeyboardAwareScrollView };
